a1a40579f4bdee23e76e9502e3416700d4aa0bfb
livemehere <rhdxoals@gmail.com>
2023-09-18 00:37:32 +0900

    client-fix: vite pwa

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
@@ -11,6 +12,34 @@ export default ({ mode }) => {
       react(),
       svgr({
         exportAsDefault: true,
+      }),
+      VitePWA({
+        registerType: "autoUpdate",
+        devOptions: {
+          enabled: true,
+        },
+        injectRegister: "auto",
+        workbox: {
+          globPatterns: ["**/*.{js,css,html,png,jpg,svg}"],
+        },
+        manifest: {
+          name: "Kong's Discord",
+          short_name: "Kong's Discord",
+          description: "Kong's Discord",
+          theme_color: "#1E1F22",
+          icons: [
+            {
+              src: "public/icons/icon-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "public/icons/icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
       }),
     ],
     resolve: {
